030b2ac1c833fd2b2eb4db1078c7cdc5b61d304b
Natalia <93074346+natasyo@users.noreply.github.com>
2024-11-18 14:00:43 +0300

    add files

---
+++ src/js/modules/mobile-nav.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+function mobileNav() {
+	// Mobile nav button
+	const navBtn = document.querySelector('.mobile-nav-btn');
+	const nav = document.querySelector('.mobile-nav');
+	const menuIcon = document.querySelector('.nav-icon');
+
+	navBtn.onclick = function () {
+		nav.classList.toggle('mobile-nav--open');
+		menuIcon.classList.toggle('nav-icon--active');
+		document.body.classList.toggle('no-scroll');
+	};
+	console.log($('div')[0])
+}
+
+export default mobileNav;
