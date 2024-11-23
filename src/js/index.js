b6fe562cd981a40025d893a7554e8013b9f3bf52
natasyo <natasyo23@gmail.com>
2024-11-23 22:52:48 +0300

    feat:broad

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
 import mobileNav from './modules/mobile-nav.js';
+
 mobileNav();
+
+import './modules/benefit.js'
---
+++ src/js/modules/benefit.js
@@ -0,0 +1,4 @@
+import $ from 'jquery'
+import 'slick-slider'
+
+$('.benefit__items').slick()
