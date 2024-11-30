32ffb8b2b644fcc72e358f1c7f5acce26bfdca6d
natasyo <natasyo23@gmail.com>
2024-11-30 11:31:36 +0300

    feat: map

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,13 +1,37 @@
-import $ from "jquery"
-let map;
+ymaps.ready(function () {
+  var myMap = new ymaps.Map(
+      "map",
+      {
+        center: [55.751574, 37.573856],
+        zoom: 9,
+      },
+      {
+        searchControlProvider: "yandex#search",
+      }
+    ),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: red; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: "Собственный значок метки",
+        balloonContent: "Это красивая метка",
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#image",
+        // Своё изображение иконки метки.
+        // iconImageHref: "images/myIcon.gif",
+        // Размеры метки.
+        iconImageSize: [30, 42],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38],
+      }
+    );
 
-async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-
-    map = new Map($(('.map__content')), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
-
-initMap();
+  myMap.geoObjects.add(myPlacemark);
+});
