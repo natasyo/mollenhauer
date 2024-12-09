41e46d5127b81f77312b1b78d557b26f90cd261f
natasyo <natasyo23@gmail.com>
2024-12-09 20:37:57 +0300

    fix: desk

--- src/js/modules/broad.js
+++ src/js/modules/broad.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
 
 if ($(window).width() <= 1280) {
-  $(".broad__items").slick({ slidesToShow: 4, slidesToScroll: 4 });
+  $(".broad__items").slick({ slidesToShow: 4, slidesToScroll: 4, dots: true });
 }
