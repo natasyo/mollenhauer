0bfdc497f4b995dd05fd3d871ddff35276142658
Natalia <natasyo23@gmail.com>
2024-12-11 23:25:08 +0300

    fix: images

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -2,8 +2,6 @@ import $ from "jquery";
 function mobileNav() {
   $(".mobile-nav-btn").on("click", function (e) {
     $("body").toggleClass("mobile-nav-show");
-    // $(e.target).toggleClass('mobile-nav-btn_is-open')
-    // $('.mobile-menu').toggleClass('mobile-menu_is-open')
   });
 }
 
