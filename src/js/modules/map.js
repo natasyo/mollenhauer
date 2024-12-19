875e2349a7f45c4676e537892f64c4b7651dae8c
Natalia <natasyo23@gmail.com>
2024-12-19 13:13:06 +0300

    feat: without map

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,39 +1,36 @@
 
 
-ymaps.ready(function () {
-  var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [55.751574, 37.573856],
-        zoom: 9,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    ),
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: red; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: "Собственный значок метки",
-        balloonContent: "Это красивая метка",
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#image",
-        // Своё изображение иконки метки.
-        // iconImageHref: "images/myIcon.gif",
-        // Размеры метки.
-        iconImageSize: [30, 42],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38],
-      }
-    );
+// ymaps.ready(function () {
+//   var myMap = new ymaps.Map(
+//       "map",
+//       {
+//         center: [55.751574, 37.573856],
+//         zoom: 9,
+//       }
+//     ),
+//     // Создаём макет содержимого.
+//     MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+//       '<div style="color: red; font-weight: bold;">$[properties.iconContent]</div>'
+//     ),
+//     myPlacemark = new ymaps.Placemark(
+//       myMap.getCenter(),
+//       {
+//         hintContent: "Собственный значок метки",
+//         balloonContent: "Это красивая метка",
+//       },
+//       {
+//         // Опции.
+//         // Необходимо указать данный тип макета.
+//         iconLayout: "default#image",
+//         // Своё изображение иконки метки.
+//         // iconImageHref: "images/myIcon.gif",
+//         // Размеры метки.
+//         iconImageSize: [30, 42],
+//         // Смещение левого верхнего угла иконки относительно
+//         // её "ножки" (точки привязки).
+//         iconImageOffset: [-5, -38],
+//       }
+//     );
 
-  myMap.geoObjects.add(myPlacemark);
-});
+//   myMap.geoObjects.add(myPlacemark);
+// });
