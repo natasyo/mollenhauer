0999f20a846b59a3a18eabdb73e5f54517c48775
Natalia <natasyo23@gmail.com>
2024-12-19 18:34:04 +0300

    fix: menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+import $ from "jquery";
 import mobileNav from "./modules/mobile-nav.js";
 
 mobileNav();
@@ -6,3 +7,18 @@ import "./modules/benefit.js";
 import "./modules/key-player.js";
 import "./modules/map.js";
 import "./modules/broad.js";
+
+
+ 
+	var nav = $('.header');
+    console.log(nav.height())
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > nav.height()) {
+           $('.mobile-nav-btn').addClass("f-nav");
+			nav.addClass("f-nav");
+		} else {
+            $('.mobile-nav-btn').removeClass("f-nav");
+            nav.removeClass("f-nav");
+		}
+	});
+ 
