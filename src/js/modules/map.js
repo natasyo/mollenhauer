ec1937e36a29a425b1b2f67c5ff8d20f83afa43a
natasyo <natasyo23@gmail.com>
2024-12-23 17:27:49 +0300

    fix: lazy load

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,36 +1,35 @@
+import $ from "jquery";
+const YaMapsShown = false;
+$(window).scroll(function () {
+  if (!YaMapsShown) {
+    if (
+      $(window).scrollTop() + $(window).height() >
+      $(document).height() - 100
+    ) {
+      ymaps.ready(function () {
+        var myMap = new ymaps.Map("map", {
+            center: [55.751574, 37.573856],
+            zoom: 9,
+          }),
+          MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: red; font-weight: bold;">$[properties.iconContent]</div>'
+          ),
+          myPlacemark = new ymaps.Placemark(
+            myMap.getCenter(),
+            {
+              hintContent: "Собственный значок метки",
+              balloonContent: "Это красивая метка",
+            },
+            {
+              iconLayout: "default#image",
+              iconImageSize: [30, 42],
+              iconImageOffset: [-5, -38],
+            }
+          );
 
-
-ymaps.ready(function () {
-  var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [55.751574, 37.573856],
-        zoom: 9,
-      }
-    ),
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: red; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: "Собственный значок метки",
-        balloonContent: "Это красивая метка",
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#image",
-        // Своё изображение иконки метки.
-        // iconImageHref: "images/myIcon.gif",
-        // Размеры метки.
-        iconImageSize: [30, 42],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-5, -38],
-      }
-    );
-
-  myMap.geoObjects.add(myPlacemark);
+        myMap.geoObjects.add(myPlacemark);
+      });
+      YaMapsShown = true;
+    }
+  }
 });
